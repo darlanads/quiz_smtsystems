6288f0a778be1863dcba2e5408d5b88dec897568
Darlan Araújo dos Santos <darlan.ads@gmail.com>
2017-07-06 13:30:17 -0300

    Alterado para aplicação do teste.

--- src/br/com/smtsystems/exercicio4/model/Fibonacci.java
+++ src/br/com/smtsystems/exercicio4/model/Fibonacci.java
@@ -3,6 +3,6 @@ package br.com.smtsystems.exercicio4.model;
 public class Fibonacci {
 
 	public static int fibonacci(int n) {
-        return (n < 2) ? n : fibonacci(n - 1) + fibonacci(n - 2);
+        return fibonacci(n - 1) + fibonacci(n - 2);
     }
 }
