13aa8940344374fc448b9d161be9e7a002eab854
Darlan Araújo dos Santos <darlan.ads@gmail.com>
2017-07-06 10:59:35 -0300

    Ajustes.

--- src/br/com/smtsystems/exercicio1/model/Posicao.java
+++ src/br/com/smtsystems/exercicio1/model/Posicao.java
@@ -31,7 +31,6 @@ public class Posicao {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + coluna;
 		result = prime * result + linha;
 		return result;
 	}
